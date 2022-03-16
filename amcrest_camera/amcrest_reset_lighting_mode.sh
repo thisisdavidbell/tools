9a3890e74238e99c53490899ae5be65c9b735dd2
thisisdavidbell <thisisdavidbell@hotmail.com>
2022-03-16 21:41:15 +0000

    minor updates to amcrest script

--- amcrest_camera/amcrest_reset_lighting_mode.sh
+++ amcrest_camera/amcrest_reset_lighting_mode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleepDuration=30
+sleepDuration=10
 
 usage() {
     printf "\n"
@@ -12,13 +12,12 @@ usage() {
 }
 
 getState() {
-    state=$(curl -s --digest -u "admin:$AMCRESTPASSWORD" "http://${IPADDRESS}/cgi-bin/configManager.cgi?action=getConfig&name=Lighting" | grep Mode | cut -d"=" -f2 | tr -d '[:space:]')
+    state=$(curl -s --digest -m 5 -u "admin:$AMCRESTPASSWORD" "http://${IPADDRESS}/cgi-bin/configManager.cgi?action=getConfig&name=Lighting" | grep Mode | cut -d"=" -f2 | tr -d '[:space:]')
     printf "${state}"
 }
 
 setState() {
-    curl -g -s --digest -u "admin:$AMCRESTPASSWORD" "http://${IPADDRESS}/cgi-bin/configManager.cgi?action=setConfig&Lighting[0][0].Mode=Manual"
-    printf "${state}"
+    curl -g -s --digest -m 5 -u "admin:$AMCRESTPASSWORD" "http://${IPADDRESS}/cgi-bin/configManager.cgi?action=setConfig&Lighting[0][0].Mode=Manual"
 }
 
 if [[ -z "${IPADDRESS}" ]]; then
@@ -33,7 +32,7 @@ for (( ; ; )); do
     state=$(getState)
 
     if [[ "${state}" == "SmartLight" ]]; then
-        printf "state = bad value: %s\n" "${state}"
+        printf "Detected bad value: %s\n" "${state}"
         printf "Executing curl command to update Mode to manual...\n"
         setState
 
@@ -41,9 +40,11 @@ for (( ; ; )); do
         if [[ "${updatedState}" != "Manual" ]]; then
             printf "state still not Manual, is: %s\n" "${updatedState}"
         else
-            printf "state successfully updated to Manual, is: %s\n" "updatedState"
+            printf "state successfully updated, is: %s\n" "${updatedState}"
 
         fi
+    elif [[ "${state}" == "" ]]; then
+        printf "State was blank, CAMERA WENT DOWN (most likely)\n"
     else
         printf "State was not SmartLight, was: %s\n" "${state}"
     fi
